9f801a5ed4d9ddc6ab54690754ce7159dd78080d
Jorge Romo <andresrt952@gmail.com>
2023-07-05 09:15:20 -0500

    mejorando la suma de productos

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,13 @@
-
-
 export const PrecioTotal = (productos) => {
-    let total = 0
+    let total = 0; // Variable para almacenar el total inicializado en 0
+
     productos.forEach((producto) => {
-        total += producto.precio
-    })
-    return total
-}
+        const precio = parseFloat(producto.precio); // Convertir el precio a número decimal
+
+        if (!isNaN(precio)) { // Verificar si el precio es un número válido (no es NaN)
+            total += precio; // Sumar el precio al total
+        }
+    });
+
+    return total.toFixed(2); // Devolver el total formateado con dos decimales
+};
